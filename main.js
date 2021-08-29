2a7b87b31b0b7448df1981fabfd2039387356803
RevathySanthanam97 <revathysanthanam97@gmail.com>
2021-08-29 14:08:59 +0530

    updated

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-var x = Object.assign([], $("#cardNumOp").html());
-var xx = new Array();
+var cardNumOp = Object.assign([], $("#cardNumOp").html());
+var cardNumOpArr = new Array();
 for (y = 2021; y <= 2050; y++) {
   var optn = document.createElement("OPTION");
   optn.text = y;
   optn.value = y;
-  document.getElementById("year").options.add(optn);
+  document.getElementById("cardYearIp").options.add(optn);
 }
 var d = new Date();
 var monthArray = new Array();
@@ -26,56 +26,94 @@ for (m = 0; m <= 11; m++) {
   var optn = document.createElement("OPTION");
   optn.text = monthArray[m];
   optn.value = m + 1;
-  document.getElementById("month").options.add(optn);
+  document.getElementById("cardMntIp").options.add(optn);
 }
-document.getElementById("cvvInput").addEventListener("click", function () {});
+document.getElementById("cvvIp").addEventListener("click", function () {});
 $(".inputField").click(function (e) {
-  if (e.target.id == "cvvInput") {
+  if (e.target.id == "cvvIp") {
     $("#card").addClass("active");
   } else {
     $("#card").removeClass("active");
   }
 });
+
 $("input").keyup(function (e) {
-  if (e.target.value.length > 0) {
-    $(this).val(
-      $(this)
-        .val()
-        .replace(/[^0-9]/g, "")
-        .match(/.{1,4}/g)
-        .join(" ")
-    );
-  }
-
-  var y = e.target.value.split("");
-
-  y.map((item, index) => {
-    if (
-      e.target.value.length - 1 == index &&
-      !(
-        e.target.value.length == 4 ||
-        e.target.value.length == 9 ||
-        e.target.value.length == 14
-      )
-    ) {
-      x[index] = `<span class="active ${index}">${item}</span>`;
-    } else {
-      x[index] = `<span class="${index}">${item}</span>`;
+  var elem = this.id;
+  var printOutput = this.value;
+  if (elem == "cardNumIp") {
+    findCard(this.value);
+    this.value = this.value.replace(/[^0-9.]/g, "").replace(/(\..*)\./g, "$1");
+    if (this.value.length > 0) {
+      $(this).val(
+        $(this)
+          .val()
+          .replace(/[^0-9]/g, "")
+          .match(/.{1,4}/g)
+          .join(" ")
+      );
     }
-  });
-  if (e.key === "Backspace" || e.key === "Delete") {
-    if (
-      !(
-        e.target.value.length == 4 ||
-        e.target.value.length == 9 ||
-        e.target.value.length == 14
-      )
-    ) {
-      x.splice(e.target.value.length, 1, "#");
-    } else {
-      x.splice(e.target.value.length + 1, 1, "#");
+    if (e.key === "Backspace" || e.key === "Delete") {
+      findCard(this.value);
+      if (
+        !(
+          this.value.length == 4 ||
+          this.value.length == 9 ||
+          this.value.length == 14
+        )
+      ) {
+        cardNumOp.splice(this.value.length, 1, "#");
+      } else {
+        cardNumOp.splice(this.value.length + 1, 1, "#");
+      }
     }
+    var y = this.value.split("");
+
+    y.map((item, index) => {
+      if (this.value.length - 1 == index) {
+        cardNumOp[index] = `<span class="active ${index}">${item}</span>`;
+      } else {
+        cardNumOp[index] = `<span class="${index}">${item}</span>`;
+      }
+    });
+    var printOutput = cardNumOp.join("");
+  } else if (elem == "cvvIp") {
+    printOutput = printOutput.replace(/[0-9]/g, "*");
   }
-  var xx = x.join("");
-  $("#" + e.target.id.slice(0, -2) + "Op").html(xx);
+  printVal(elem, printOutput);
 });
+$("select").change(function (e) {
+  var elem = this.id;
+  var printOutput = this.value;
+  if (this.value.length < 2) {
+    printOutput = "0" + this.value;
+  }
+  if (elem == "cardYearIp") {
+    printOutput = this.value.substr("-2");
+  }
+  printVal(elem, printOutput);
+});
+
+function printVal(elem, printValue) {
+  $("#" + elem.slice(0, -2) + "Op").html(printValue);
+}
+function findCard(val) {
+  if ((val.length >= 2 && val.substr(0, 2) == 34) || val.substr(0, 2) == 37) {
+    console.log(val.substr(0, 2));
+    $(".cardBrand").attr("src", "./images/unionpay.png");
+  } else if (
+    (val.length >= 2 && val.substr(0, 2) == 51) ||
+    val.substr(0, 2) == 52 ||
+    val.substr(0, 2) == 53 ||
+    val.substr(0, 2) == 54 ||
+    val.substr(0, 2) == 55
+  ) {
+    $(".cardBrand").attr("src", "./images/mastercard.png");
+  } else if (
+    (val.length >= 4 && val.substr(0, 4) == 2131) ||
+    val.substr(0, 2) == 1800
+  ) {
+    $(".cardBrand").attr("src", "./images/mastercard.png");
+  } else {
+    $(".cardBrand").attr("src", "./images/visa.png");
+  }
+}
